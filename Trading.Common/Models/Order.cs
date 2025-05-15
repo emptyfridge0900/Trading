b85d1b6109fb8ca4effa15f289fad2ff24e22a3c
Doosan <emptyfridge0900@gmail.com>
2025-05-15 13:38:37 +0900

    Adding a library that share common classes

---
+++ Trading.Common/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Trading.Common.Models
+{
+    public class Order
+    {
+        public string Name {  get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Trading.Common/Models/Ticker.cs
@@ -0,0 +1,27 @@
+﻿namespace Trading.Common.Models
+{
+    public class Ticker
+    {
+        private float _price;
+        public string Symbol { get; init; }
+        public float Price 
+        {
+            get { return _price; }
+            set
+            {
+                if(_price==0)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    Changes = value - _price;
+                    ChangePercent = Changes / _price * 100;
+                    _price = value;
+                }       
+            }
+        }
+        public float Changes { get; private set; }
+        public float ChangePercent { get; private set; }
+    }
+}
