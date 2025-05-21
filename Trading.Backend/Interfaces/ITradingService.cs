54a9f515b067371d07b0692a565dfd8fecf2d730
Doosan <emptyfridge0900@gmail.com>
2025-05-21 11:15:25 +0900

    Adding comment about Bid, Ask method

--- Trading.Backend/Interfaces/ITradingService.cs
+++ Trading.Backend/Interfaces/ITradingService.cs
@@ -11,7 +11,28 @@ namespace Trading.Backend.Interfaces
         /// <returns></returns>
         Task<List<TradeRecord>> GetTradingRecords(string userId);
         Task AddRecord(TradeRecord record);
+        /// <summary>
+        /// Verify whether any asking price is higher than the bidding price. 
+        /// If such a case exists, remove the corresponding ask from the AskQueue 
+        /// and enqueue the remaining orders into the BidQueue.
+        /// </summary>
+        /// <param name="userId">Uniquely identified name</param>
+        /// <param name="symbol">Ticker symbol</param>
+        /// <param name="price">Price to bid</param>
+        /// <param name="quantity">Quantity to bid</param>
+        /// <returns></returns>
         Task Bid(string userId, string symbol, float price, int quantity);
+
+        /// <summary>
+        /// Verify whether any bidding price is higher than the asking price. 
+        /// If such a case exists, remove the corresponding bid from the BidQueue 
+        /// and enqueue the remaining orders into the AskQueue.
+        /// </summary>
+        /// <param name="userId">Uniquely identified name</param>
+        /// <param name="symbol">Ticker symbol</param>
+        /// <param name="price">Price to ask</param>
+        /// <param name="quantity">Quantity to ask</param>
+        /// <returns></returns>
         Task Ask(string userId, string symbol, float price, int quantity);
 
     }
